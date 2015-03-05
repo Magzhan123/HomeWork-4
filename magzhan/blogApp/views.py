7fc75f119cba2b2d52a2a8c155a9506b772e3607
Magzhan <magzhan.sarzhan@is.sdu.edu.kz>
2015-03-05 22:41:57 +0600

    I have created models and fields

---
+++ magzhan/blogApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from polls.models import HttpResponse
+from blogApp.models import Post
+
+def postShow(request, blog_id):
+	return HttpResponse("We are in post with id= %s" %blog_id )
+
+def index(request):
+    blog_id = HttpResponse.objects.order_by('-pub_date')[:5]
+    context = {'blog_id': blog_id}
+    return render(request, 'blogApp/index.html', context)
